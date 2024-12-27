b8b362ac90e2bfad8b5766429805bfc88cabf6dd
Joabe Soares Teixeira de Souza <joabe.soares1@gmail.com>
2024-12-27 15:40:00 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Função para abrir o menu lateral
+function openNav() {
+    document.getElementById("menu").style.width = "250px";
+    document.body.style.marginLeft = "250px";
+}
+
+// Função para fechar o menu lateral
+function closeNav() {
+    document.getElementById("menu").style.width = "0";
+    document.body.style.marginLeft = "0";
+}
